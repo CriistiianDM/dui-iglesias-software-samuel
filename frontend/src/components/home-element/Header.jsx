ffaf78dd2fbb027f06d315c9fcfb93a3c739a039
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-04-30 21:04:54 -0500

    creacion del header con material iu y responsive

--- frontend/src/components/home-element/Header.jsx
+++ frontend/src/components/home-element/Header.jsx
@@ -1,7 +1,9 @@
 //libreary or imports
 import React from "react";
-import { Button } from '@material-ui/core';
-import header_home from '../../css/header_home.css'
+import { Button, IconButton } from '@material-ui/core';
+import header_home from '../../css/header_home.css';
+import logoIglesia  from '../../images/imagesHome/logo_iglesia.png';
+import MenuIcon from '@material-ui/icons/Menu';
 
 /**
   *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
@@ -15,7 +17,15 @@ export function Header(props) {
         <>
 
          <header className={state_header['cls-1']}>
-             <Button> click </Button>
+
+            <Button target="_blank" href="https://ipuc.org.co" className={state_header['cls-2']}> 
+                <img className={state_header['cls-3']} alt="" src={logoIglesia} /> 
+            </Button>
+           
+            <IconButton  className={state_header['cls-4']} aria-label="menu">
+                <MenuIcon fontSize="large"  />
+            </IconButton>
+
          </header>
 
         </>
