59e36155e5249cf8e49b3aef234c472f8088674a
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-05-01 00:07:49 -0500

    creacion responsive de header y footer

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 //librerarys or imports
 import React from 'react';
 import { Header } from '../home-element/Header';
+import { Footer } from '../home-element/Footer';
 
 
 
@@ -12,11 +13,13 @@ export function Home(props) {
 
   //state of componets
   let state_header = Object.values(Object.values(Object.entries(props)[0][1])[0])[0];
-  
+  let state_footer = Object.values(Object.values(Object.entries(props)[0][1])[1])[0];
+  console.log(state_footer,'state_footer');
 
   return (
       <>
         <Header properties={state_header} />
+        <Footer properties={state_footer} />
       </>
   );
   
