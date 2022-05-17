18d16ea93350bd9d7c0c45fedc7ebd520c2d1523
JuanSe320 <63017408+JuanSe320@users.noreply.github.com>
2022-05-16 21:44:32 -0500

    nueva implementacion del login

--- frontend/src/components/login-element/TextBoxs.jsx
+++ frontend/src/components/login-element/TextBoxs.jsx
@@ -1,6 +1,7 @@
 //libreary or imports
 import React from "react";
 import login_style from '../../css/login_style.css';
+import TextField from '@material-ui/core/TextField';
 
 /**
   *  @author : Juan Sebastian Camino Muñoz<juan.camino@correounivalle.edu.co>
@@ -11,15 +12,13 @@ export function TextBoxs(props) {
     let state_textboxs  = props.properties;
     
     return (
-        <>
-         <div className={state_textboxs['cls-4']}>
-           <div className={state_textboxs['cls-5']}>
-            caja1
-           </div>
-           <div className={state_textboxs['cls-6']}>
-            caja2
-           </div>
-         </div>
-        </>
+      <>
+        <form noValidate autoComplete="off">
+           <TextField className={state_textboxs['cls-5']} label="Usuario" fullWidth variant="outlined">
+           </TextField>
+           <TextField className={state_textboxs['cls-5']} label="contraseña" fullWidth variant="outlined">
+           </TextField>
+        </form>
+      </>
     );
 }
