325b3c12fa00be659b9bbe7f5d3c386043902a14
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-05-27 19:42:50 -0500

    se terminaron de crear los botoes dinamicos de las diferentes opcines

--- frontend/src/components/account-element/SettingAccounts.jsx
+++ frontend/src/components/account-element/SettingAccounts.jsx
@@ -1,26 +1,50 @@
 //librerary or import of react
-import { Button , Typography } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 import React from 'react';
 import settingStyle from '../../css/settingStyle.css';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
-
+import Icon from '@material-ui/core/Icon';
 
 export function SettingAccounts(props) {
 
    let state_Setting_accounts = props.properties;
 
+   //console.log( (state_Setting_accounts['botones']).map( (boton) => (boton.id) )  );
+
    return (
-    
-     <div className={state_Setting_accounts['cls-1']}>
-          <Button className={state_Setting_accounts['cls-2']}>
-              <div className={state_Setting_accounts['cls-3']}>
-                 <PersonAddIcon className={state_Setting_accounts['cls-4']} />
-                 <Typography> HOLA</Typography>
-              </div>
-          </Button>
-          <Button className={state_Setting_accounts['cls-2']}>2</Button>
-          <Button className={state_Setting_accounts['cls-2']}>3</Button>
-     </div>
+
+      <div className={state_Setting_accounts['cls-1']}>
+
+         {
+            (state_Setting_accounts['botones']).map((boton) => (
+
+               <Button key={boton.id} className={state_Setting_accounts['cls-2']} id={boton.id}>
+
+                  <div className={state_Setting_accounts['cls-3']}>
+
+                     <Icon  className={state_Setting_accounts['cls-4']}>{boton.icon}</Icon>
+
+                     <div className={state_Setting_accounts['cls-5']}>
+
+                        <Typography  className={state_Setting_accounts['cls-6']}>
+                           {boton.title}
+                        </Typography>
+
+                        <Typography  className={state_Setting_accounts['cls-7']}>
+                           {boton.content}
+                        </Typography>
+
+                     </div>
+
+                  </div>
+
+               </Button>
+
+            ))
+
+         }
+
+      </div>
 
    )
 
