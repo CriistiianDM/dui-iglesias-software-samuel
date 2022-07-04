27e593877341f7014947552f0da07da9b789a7d2
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-07-04 16:06:57 -0500

    conexion a bd

--- backend/index/config.js
+++ backend/index/config.js
@@ -5,11 +5,12 @@ config();
 //exportar modulo
 module.exports = {
     db: {
+        /*
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        port: process.env.DB_PORT,
+        port: process.env.DB_PORT,*/
         connectionString: process.env.DB_CONNECTION_STRING
     }
 };
