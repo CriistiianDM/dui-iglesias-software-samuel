59fa8e87b3ba17162dc2c76e9b61ea8503f7147b
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-07-11 13:50:17 -0500

    ya quedo listo pa un despliegue

--- src/components/pages/CreateUser.jsx
+++ src/components/pages/CreateUser.jsx
@@ -11,14 +11,21 @@ import { CreateUserForm } from '../createuser-element/CreateUserForm';
 */
 export function CreateUser(props) {
 
-    let state_header_user = Object.values(Object.values(Object.entries(props)[0][1])[0])[1];
     let state_create_user_form = Object.values(Object.values(Object.entries(props)[0][1])[8])[0];
+
+    //useEstado
+    const [header_user, setHeaderUser] = React.useState({
+        state_header_user: Object.values(Object.values(Object.entries(props)[0][1])[0])[1],
+        nombre_persona: localStorage.getItem('user_name'),
+    });
+
+
 
     //render
     return (
         <>
-            <HeaderUser properties={state_header_user} />
-            <div className={state_header_user['cls-6']}></div>
+            <HeaderUser properties={header_user} />
+            <div className={header_user.state_header_user['cls-6']}></div>
             <CreateUserForm properties={state_create_user_form} />
         </>
     );
