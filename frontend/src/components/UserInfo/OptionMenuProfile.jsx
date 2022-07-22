6c299271b820c89adf9789d443b7016474c83a63
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-07-22 13:52:43 -0500

    se creo la funcionalidad de cargar a info del usuario hay cositas que hay que mejorar

--- frontend/src/components/UserInfo/OptionMenuProfile.jsx
+++ frontend/src/components/UserInfo/OptionMenuProfile.jsx
@@ -3,9 +3,26 @@ import { IconButton, Icon, Typography, Button } from '@material-ui/core';
 import React from 'react';
 import option_menu_profile from '../../css/option_menu_profile.css';
 
+
+//18 personal information
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : option menu profile component
+*/
 export function OptionMenuProfile(props) {
 
     let state_option_menu_profile = props.properties;
+
+    //set data to view
+    let [data_array, set_data_array] = React.useState({
+        loading: false,
+        data: []
+    });
+
+    React.useEffect(() => {
+        getData(data_array, set_data_array);
+    }, []);
 
     return (
 
@@ -62,13 +79,23 @@ export function OptionMenuProfile(props) {
 
                 <div className={state_option_menu_profile['cls-10']}>
 
-                    {
+                    {   (!data_array.loading)?
                         (state_option_menu_profile['info-personal']).map((value) => (
                             <div key={value.title} className={state_option_menu_profile['cls-8']}>
                                 <Typography className={state_option_menu_profile['cls-5']}>{value.title}</Typography>
                                 <input value={value.content} disabled className={state_option_menu_profile['cls-9']} />
                             </div>
                         ))
+                        :
+                        ( Object.entries(data_array.data)).map((value,index) => (
+                            (index !== 0 && index <= 12)? (
+                            <div key={index} className={state_option_menu_profile['cls-8']}>
+                                <Typography className={state_option_menu_profile['cls-5']}>{value[0]}</Typography>
+                                <input value={value[1]} disabled className={state_option_menu_profile['cls-9']} />
+                            </div>
+                            ) : null
+                        ))
+
                     }
 
                 </div>
@@ -77,13 +104,23 @@ export function OptionMenuProfile(props) {
                 <div className={state_option_menu_profile['cls-10']}>
 
                     {
+                        (!data_array.loading)? 
                         (state_option_menu_profile['info-eclesiastica']).map((value) => (
                             <div key={value.title} className={state_option_menu_profile['cls-8']}>
                                 <Typography className={state_option_menu_profile['cls-5']}>{value.title}</Typography>
                                 <input value={value.content} disabled className={state_option_menu_profile['cls-9']} />
                             </div>
                         ))
-                    }                  
+                        :
+                        ( Object.entries(data_array.data)).map((value,index) => (
+                            (index > 14 && index !== 24 && index !== 18)? (
+                            <div key={index} className={state_option_menu_profile['cls-8']}>
+                                <Typography className={state_option_menu_profile['cls-5']}>{value[0]}</Typography>
+                                <input value={value[1]} disabled className={state_option_menu_profile['cls-9']} />
+                            </div>
+                            ) : null
+                        ))
+                    }
 
                 </div>
                 <Typography className={state_option_menu_profile['cls-5']}>PERFIL OCUPACIONAL</Typography>
@@ -97,7 +134,7 @@ export function OptionMenuProfile(props) {
                             </div>
                         ))
                     }
-                   
+
 
                 </div>
             </div>
@@ -105,5 +142,31 @@ export function OptionMenuProfile(props) {
         </div>
 
     )
+
+}
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : get data from local storage
+*/
+function getData(data_array, set_data_array) {
+
+    
+    const timer = setInterval(() => {
+
+    let data = localStorage.getItem('user_info_eye');
+
+    if (data != null) {
+        clearInterval(timer);
+        console.log( Object.entries(JSON.parse(data)).forEach((value) => {
+            console.log(value);
+        }));
+        set_data_array({...data_array, loading: true, data: JSON.parse(data)});
+    } 
+
+    console.log('get data');
+    }, 1000);
+
+    return timer;
 
 }
