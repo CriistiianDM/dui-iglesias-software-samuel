482f1fe16fb7c0ae71fd2d51b3f35270fa6c5a67
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-08-09 23:05:02 -0500

    implementacion de la nueva interfaz

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,5 +1,7 @@
 //librerarys or imports
 import React from 'react';
+import { NewHome } from '../home-element/NewHome';
+import new_home_css from '../../css/new_home.css';
 
 
 /**
@@ -7,11 +9,12 @@ import React from 'react';
   *  @decs  : home de la aplicacion /
 */
 export function Home(props) {
-
+ 
+  const state_new_home = ((props.properties).state_home)['2'];
 
   return (
       <>
-       
+       <NewHome properties={state_new_home} />
       </>
   );
   
