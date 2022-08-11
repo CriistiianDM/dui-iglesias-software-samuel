b855747566706c444ab348200ceacd65d5e4f7f8
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-08-10 20:44:47 -0500

    changes

--- frontend/src/components/login-element/Loginbody.jsx
+++ frontend/src/components/login-element/Loginbody.jsx
@@ -206,7 +206,7 @@ async function fetch_data_login(user_valid_data, setUserValid, navigate) {
       const response_fetch = await fetch(`https://demon789-4.herokuapp.com/zcvg/${user_valid_data.user_login}`, {
         method: 'GET',
         headers: {
-          'Content-Type': 'text/plain',
+          'Content-Type': 'application/json',
           'Authorization': generateToken()
         }
       });
