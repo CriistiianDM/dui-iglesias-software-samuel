bbe36c50f1b44511e7479485074c80b827496909
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-08-12 15:15:38 -0500

    creacion de la parte grafica no tienes grupo

--- frontend/src/components/person-normal/Groups.jsx
+++ frontend/src/components/person-normal/Groups.jsx
@@ -2,46 +2,72 @@
 import { Icon, Typography } from "@material-ui/core";
 import React from "react";
 import styleGroup from "../../css/style_group.css";
-import logoIglesia  from '../../images/imagesHome/logo_iglesia.png';
+import logoIglesia from '../../images/imagesHome/logo_iglesia.png';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Montserrat_ExtraBold from '../../static/Montserrat-ExtraBold.ttf';
 
 //bienvenida a casa github copilot, te heche de menos, de verdad que te heche de menos, te quiero mucho
 
 
+//estilos de los componentes
+const useStyles = makeStyles((theme) => ({
+
+    titleGroupAux: {
+        //espacio entre el titulo y el texto
+        marginTop: '1em',
+        fontSize: '4em',
+        color: 'hsl(198deg 32% 16%)',
+        fontWeight: '600',
+        fontFamily: Montserrat_ExtraBold,
+    },
+    iconStyleGroup: {
+        height: '0.8em',
+        color: 'hsl(198deg 32% 16%)',
+        fontSize: '15em',
+    },
+    textGroupAux: {
+        paddingBottom: '1em',
+        color: 'hsl(198deg 32% 16%)',
+        fontSize: '2em',
+        fontWeight: '500',
+        fontFamily: Montserrat_ExtraBold,
+    }
+
+}));
+
+
+
+
+/**
+ * @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+ * @des grupo de la aplicacion /Account
+ * @param {json} props 
+ * @returns 
+ */
 export function Groups(props) {
 
     //variable de estado
     let state_group = props.properties;
+    const classes = useStyles();
 
+    //use state para obtener los datos del json
+    const [data_array, set_data_array] = React.useState({
+        loading: false,
+    });
+
+    //como un fressby va y vuelve - es una cansion githubcopilot
     return (
 
         <div className={state_group['cls-1']}>
-
-
-            <div className={state_group['cls-2']}>
- 
-                <div  className={state_group['cls-3']}>
-                    <img alt="" src={logoIglesia} className={state_group['cls-4']} />
-                </div>
-
-                <div  className={state_group['cls-5']}>
-
-                    <div className={state_group['cls-8']}>
-                    <Typography  className={state_group['cls-6']}>Perteneces a Un grupo</Typography>
+            <Typography className={classes.titleGroupAux}> Tus Grupos </Typography>
+            {
+                (!data_array.loading) ? (
+                    <div className={state_group['cls-9']}>
+                        <Icon className={classes.iconStyleGroup}>groups</Icon>
+                        <Typography className={classes.textGroupAux}> No Perteneces a Ningun Grupo </Typography>
                     </div>
-
-                    <div className={state_group['cls-8']} id="descrision-grupo">
-                    <Typography  className={state_group['cls-7']}>{
-                        //generar una descripcion del grupo de cualquier cosa
-                        ""
-                    }</Typography>
-                    </div>
-
-                </div>
-               
-            </div>
-
-
-
+                ) : null
+            }
         </div>
     )
 
