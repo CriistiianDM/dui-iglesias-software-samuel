a2c65595f056a93268280d2abb9c17eb8a26405b
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-08-13 21:21:53 -0500

    creación de la nueva vista
    ya se añadió el header y footer junto con el componente que pintara los jtexfields
    
    Co-Authored-By: Juan Felipe Osorio <82562585+JFOZ1010@users.noreply.github.com>
    Co-Authored-By: JuanSe320 <63017408+JuanSe320@users.noreply.github.com>

---
+++ frontend/src/components/congrecasion_vista/PageCongrecasion.jsx
@@ -0,0 +1,18 @@
+//libreria
+import React from 'react';
+
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : Vista para ver la informacion de la iglesia
+  *
+*/
+export function PageCongrecasion(props) {
+
+
+    return (
+        <>
+        <h1>Hola sebas</h1>
+        </>
+    )
+}
